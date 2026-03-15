b5d0de1c35c1b3011775c345560a606a26bdb92d
Aijaz Ahmed <aijaz@example.com>
2026-03-15 23:38:58 +0530

    terraform updated code

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "devops_sg" {
+  name = "devops-security-group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-0a14f53a6fe4dfcd1"
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.devops_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install docker.io -y
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo docker run -d -p 80:3000 yourdockerhub/node-devops-app
+              EOF
+
+  tags = {
+    Name = "DevOps-node-terraform-project Server"
+  }
+}
